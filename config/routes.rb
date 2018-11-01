38dd7fe1497d78a468070c26157dce18bab9da85
Thomas Wilson <twilson.spqr@gmail.com>
2018-11-01 19:45:31 -0400

    Home & Help views updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-  get 'foo/bat'
-
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#hello'
 end
