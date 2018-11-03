f8116ffb40825695547babcfffd9ace8ecd0d995
Thomas Wilson <twilson.spqr@gmail.com>
2018-11-03 11:39:55 -0400

    refactored testing

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,52 +1,56 @@
 require 'rails_helper'
 # well, I get to write my own tests.  I like writing tests anyway.
+describe "static pages" do
+    before(:each) do
+      @base_title = "Ruby on Rails Tutorial Sample App"
+    end
 
-feature "home page" do
-  scenario "Sees home page and link to sources" do
-    visit static_pages_home_path
+  feature "home page" do
+    scenario "shows home page and link to sources" do
+      visit static_pages_home_path
 
-    expect(page).to have_content "Sample App"
-    expect(page).to have_link "Ruby on Rails Tutorial"
+      expect(page).to have_content "Sample App"
+      expect(page).to have_link "Ruby on Rails Tutorial"
+    end
+
+    scenario " has a title" do
+      visit static_pages_home_path
+      expect(page).to have_title "Home | #{@base_title}"
+    end
+
   end
 
-  scenario "page has a title" do
-    visit static_pages_home_path
+  feature "help page" do
+    scenario "show help page with helpful resources" do
+      visit static_pages_help_path
 
-    expect(page).to have_title 'Home | Ruby on Rails Tutorial Sample App'
+      expect(page).to have_content "Help"
+      expect(page).to have_link "Rails Tutorial help page"
+      expect(page).to have_link "Ruby on Rails Tutorial book"
+    end
+
+    scenario "has a title" do
+      visit static_pages_help_path
+
+      expect(page).to have_title "Help | #{@base_title}"
+    end
   end
 
-end
+  feature "about page" do
+    scenario "shows about page with useful information" do
+      visit static_pages_about_path
 
-feature "help page" do
-  scenario "sees help page with helpful resources" do
-    visit static_pages_help_path
+      expect(page).to have_content "About"
+      expect(page).to have_link "Ruby on Rails Tutorial"
+      expect(page).to have_link "book"
+      expect(page).to have_link "screencast series"
+      expect(page).to have_link "Ruby on Rails"
+    end
 
-    expect(page).to have_content "Help"
-    expect(page).to have_link "Rails Tutorial help page"
-    expect(page).to have_link "Ruby on Rails Tutorial book"
-  end
+    scenario "has a title " do
+      visit static_pages_about_path
 
-  scenario "page has a title" do
-    visit static_pages_help_path
-
-    expect(page).to have_title "Help | Ruby on Rails Tutorial Sample App"
-  end
-end
-
-feature "about page" do
-  scenario "sees about page with useful information" do
-    visit static_pages_about_path
-
-    expect(page).to have_content "About"
-    expect(page).to have_link "Ruby on Rails Tutorial"
-    expect(page).to have_link "book"
-    expect(page).to have_link "screencast series"
-    expect(page).to have_link "Ruby on Rails"
-  end
-
-  scenario "page has a title " do
-    visit static_pages_about_path
-
-    expect(page).to have_title 'About | Ruby on Rails Tutorial Sample App'
+      expect(page).to have_title "About | #{@base_title}"
+    end
   end
 end
