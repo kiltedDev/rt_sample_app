fca7acd3c45ee5f2537b7d9c1843bb07e19d1138
Thomas Wilson <twilson.spqr@gmail.com>
2018-11-05 13:50:40 -0500

    Integration testing

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -15,14 +15,14 @@ describe "static pages" do
 
   feature "home page" do
     scenario "shows home page and link to sources" do
-      visit static_pages_home_path
+      visit home_path
 
       expect(page).to have_content "Sample App"
       expect(page).to have_link "Ruby on Rails Tutorial"
     end
 
     scenario "has a title" do
-      visit static_pages_home_path
+      visit home_path
       expect(page).to have_title "#{@base_title}"
     end
 
@@ -30,7 +30,7 @@ describe "static pages" do
 
   feature "help page" do
     scenario "show help page with helpful resources" do
-      visit static_pages_help_path
+      visit help_path
 
       expect(page).to have_content "Help"
       expect(page).to have_link "Rails Tutorial help page"
@@ -38,7 +38,7 @@ describe "static pages" do
     end
 
     scenario "has a title" do
-      visit static_pages_help_path
+      visit help_path
 
       expect(page).to have_title "Help | #{@base_title}"
     end
@@ -46,7 +46,7 @@ describe "static pages" do
 
   feature "about page" do
     scenario "shows about page with useful information" do
-      visit static_pages_about_path
+      visit about_path
 
       expect(page).to have_content "About"
       expect(page).to have_link "Ruby on Rails Tutorial"
@@ -56,7 +56,7 @@ describe "static pages" do
     end
 
     scenario "has a title " do
-      visit static_pages_about_path
+      visit about_path
 
       expect(page).to have_title "About | #{@base_title}"
     end
@@ -64,14 +64,14 @@ describe "static pages" do
 
   feature "contact page" do
     scenario "shows contact page with some people" do
-      visit static_pages_contact_path
+      visit contact_path
 
       expect(page).to have_content "Contact"
       expect(page).to have_link "contact page"
     end
 
     scenario "has a title" do
-      visit static_pages_contact_path
+      visit contact_path
 
       expect(page).to have_title "Contact | #{@base_title}"
     end
---
+++ spec/features/nav_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'navigation', %(
+  As a user, I want to click on links and have them go to not the wrong place.
+) do
+  before(:each) do
+    visit root_path
+  end
+
+  scenario 'header should have correct links' do
+
+    expect(page).to have_link("sample app", href: root_path)
+    expect(page).to have_link("Home", href: root_path)
+    expect(page).to have_link("Help", href: help_path)
+    # expect(page).to have_link("Log In", href: about_path)
+
+  end
+
+  scenario 'Footer should have correct links' do
+    expect(page).to have_link("Ruby on Rails Tutorial", href: "http://www.railstutorial.org/")
+    expect(page).to have_link("About", href: about_path)
+    expect(page).to have_link("Contact", href: contact_path)
+  end
+
+  # scenario 'Footer should have correct links' do
+  #
+  #   expect(page).to have_link("sample app", href: root_path)
+  #   expect(page).to have_link("Home", href: root_path)
+  #   expect(page).to have_link("Help", href: help_path)
+  #   expect(page).to have_link("About", href: about_path)
+  #   expect(page).to have_link("Contact", href: contact_path)
+  # end
+end
