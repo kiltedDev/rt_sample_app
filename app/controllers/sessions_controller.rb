38b9a01348c1d0b14fb7de89673d4166434695d3
Thomas Wilson <twilson.spqr@gmail.com>
2018-11-10 20:13:21 -0500

    permanent cookies added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user
     else
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before(:each) do
-    @user = User.new( name: "Example User",
-                      email: "user@example.com",
-                      password: "password",
-                      password_confirmation: "password" )
+    @bob = create(:user)
   end
 
   it "is a valid model" do
-    expect(@user).to be_valid
+    expect(@bob).to be_valid
   end
 
   it "is not valid without a valid name" do
-    @user.name = "     "
-    expect(@user).to_not be_valid
+    @bob.name = "     "
+    expect(@bob).to_not be_valid
   end
 
   it "is not valid without a valid email address" do
-    @user.email = "     "
-    expect(@user).to_not be_valid
+    @bob.email = "     "
+    expect(@bob).to_not be_valid
   end
 
   it "is not valid without a valid password" do
@@ -27,21 +24,21 @@ RSpec.describe User, type: :model do
   end
 
   it "is not valid with too long a name" do
-  @user.name = "a" * 51
-  expect(@user).to_not be_valid
+  @bob.name = "a" * 51
+  expect(@bob).to_not be_valid
   end
 
   it "is not valid with too long a email" do
-    @user.email = "a" * 244 + "@example.com"
-    expect(@user).to_not be_valid
+    @bob.email = "a" * 244 + "@example.com"
+    expect(@bob).to_not be_valid
   end
 
   it "is valid with a properly formatted email" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
     first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
-      @user.email = valid_address
-      expect(@user).to be_valid
+      @bob.email = valid_address
+      expect(@bob).to be_valid
     end
   end
 
@@ -49,37 +46,41 @@ RSpec.describe User, type: :model do
     invalid_addresses = %w[Bill USER@foo A_US-ER&foo.bar.org
     first.last@foo,jp alice@bob+baz.cn bob@bob..com]
     invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      expect(@user).to_not be_valid
+      @bob.email = invalid_address
+      expect(@bob).to_not be_valid
     end
   end
 
   it "is not valid if an email address is already on file" do
-    duped_user = @user.dup
+    duped_user = @bob.dup
     duped_user.email = duped_user.email.upcase
-    @user.save
+    @bob.save
     expect(duped_user).to_not be_valid
   end
 
   it "saves email addresses as lower cast" do
     gamzee_email = "UsEr@ExAmPlE.cOm"
-    @user.email = gamzee_email
-    @user.save
-    expect(@user.email).to eq(@user.reload.email)
+    @bob.email = gamzee_email
+    @bob.save
+    expect(@bob.email).to eq(@bob.reload.email)
   end
 
   it "is not valid with a blank password" do
-    @user.password = @user.password_confirmation = "      "
-    expect(@user).to_not be_valid
+    @bob.password = @bob.password_confirmation = "      "
+    expect(@bob).to_not be_valid
   end
 
   it "is not valid with a too short password" do
-    @user.password = @user.password_confirmation = "1234567"
-    expect(@user).to_not be_valid
+    @bob.password = @bob.password_confirmation = "1234567"
+    expect(@bob).to_not be_valid
   end
 
   it "is not valid with a too long password" do
-    @user.password = @user.password_confirmation = "x" * 65
-    expect(@user).to_not be_valid
+    @bob.password = @bob.password_confirmation = "x" * 65
+    expect(@bob).to_not be_valid
+  end
+
+  it "is not authenticated with a nil digest" do
+    expect(@bob.authenticated?('')).to be(false)
   end
 end
